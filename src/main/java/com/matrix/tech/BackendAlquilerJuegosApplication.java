6bc9075b8a62f8b128d432af653e26adacbd4614
Amilkar Hernandez <amilkarhernandez07@gmail.com>
2020-06-19 14:09:36 -0500

    Initial commit

---
+++ src/main/java/com/matrix/tech/BackendAlquilerJuegosApplication.java
@@ -0,0 +1,13 @@
+package com.matrix.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAlquilerJuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAlquilerJuegosApplication.class, args);
+	}
+
+}
